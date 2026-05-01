d11046f85add9c5a039f0bb0f2ef1ee15443e1cb
Oleksandr Voloshyn <volosins651@gmail.com>
2026-05-02 00:44:10 +0300

    Clean

--- StructuralPatterns/StructuralPatterns/obj/Debug/net10.0/StructuralPatterns.AssemblyInfo.cs
+++ StructuralPatterns/StructuralPatterns/obj/Debug/net10.0/StructuralPatterns.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StructuralPatterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+896a4ff42160cc268ac57b5a9b4c20cb32ea680e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0a922505588fefe0ac65ead3e48e79bb60e87c")]
 [assembly: System.Reflection.AssemblyProductAttribute("StructuralPatterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StructuralPatterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
