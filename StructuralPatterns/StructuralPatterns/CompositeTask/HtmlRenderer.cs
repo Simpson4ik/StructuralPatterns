8de4c0cd123b4aeccd90f656dd861ac9eabc36b8
Oleksandr Voloshyn <volosins651@gmail.com>
2026-05-03 00:14:44 +0300

    Додано патерн Шаблонний метод для рендерингу вузлів

---
+++ StructuralPatterns/StructuralPatterns/CompositeTask/HtmlRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructuralPatterns.CompositeTask
+{
+    public abstract class HtmlRenderer
+    {
+        public string Render(LightNode node)
+        {
+            OnBeforeRender(node);
+            string html = BuildHtml(node);
+            OnAfterRender(node, html);
+
+            return html;
+        }
+
+        protected virtual void OnBeforeRender(LightNode node) { }
+
+        private string BuildHtml(LightNode node)
+        {
+            return node.OuterHTML;
+        }
+        protected virtual void OnAfterRender(LightNode node, string html) { }
+    }
+}
---
+++ StructuralPatterns/StructuralPatterns/CompositeTask/LoggableRenderer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructuralPatterns.CompositeTask
+{
+    public class LoggableRenderer : HtmlRenderer
+    {
+        protected override void OnBeforeRender(LightNode node)
+        {
+            Console.WriteLine($"[Лог] Починаємо рендер вузла...");
+        }
+
+        protected override void OnAfterRender(LightNode node, string html)
+        {
+            Console.WriteLine($"[Лог] Рендер завершено. Довжина HTML: {html.Length} символів.");
+        }
+    }
+}
